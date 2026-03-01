e667f943b884d13298ea342d188d890798690811
gabrielnatividaddev-coder <gabriel.natividad.dev@gmail.com>
2026-03-01 10:09:11 +0800

    fix

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import './Header.css';
-import { Button } from '@mantine/core';
+import {Burger, Group, Drawer, ScrollArea, Divider, UnstyledButton,Center, Box, Collapse } from '@mantine/core';
 import JBDM from  '../assets/javier.png';
 import { Link } from 'react-router-dom';
-class Header extends React.Component {
-  render(){
-    return <div className='headermain'>
+import { useDisclosure } from '@mantine/hooks';
+const Header = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+    return <div><div className='headermain'>
             <div className='leftsidediv'>
             <img className='setimage' height={30} src={JBDM}></img>
             <a className='leftside'>Gabriel </a>
             </div>
-            <div className='fouritems'>
+            <Group className='fouritems' visibleFrom="sm">
                 <Link to="/" className='item'>Home</Link>
                 <Link to='/features' className='item'>Features</Link>
                 <Link to='/about' className='item'>About</Link>
                 <Link to='/links' className='item'>Links</Link>
-            </div>
+            </Group>
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            hiddenFrom="sm"
+            aria-label="Toggle navigation"
+          />
           </div>
-  }
+          <Drawer
+            opened={drawerOpened}
+            onClose={closeDrawer}
+            size="100%"
+            padding="md"
+            title="Navigation"
+            hiddenFrom="sm"
+            zIndex={1000000}
+          >
+            <ScrollArea h="calc(100vh - 80px" mx="-md">
+                <Divider my="sm"/>
+                <Group className='divideritems'>
+                <Link to="/" className='itemdivider'>Home</Link>
+                <Link to='/features' className='itemdivider'>Features</Link>
+                <Link to='/about' className='itemdivider'>About</Link>
+                <Link to='/links' className='itemdivider'>Links</Link>
+                </Group>
+            </ScrollArea>
+          </Drawer>
+          </div>
 }
 
 export default Header;
