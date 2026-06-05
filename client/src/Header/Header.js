8158fe06931a7a6cf16c24edf432d2cc472cb22f
gabrielnatividaddev-coder <gabriel.natividad.dev@gmail.com>
2026-06-05 10:25:40 +0800

    edits

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -28,13 +28,13 @@ const Header = () => {
           <Drawer
             opened={drawerOpened}
             onClose={closeDrawer}
-            size="100%"
+            size="60%"
             padding="md"
             title="Navigation"
             hiddenFrom="sm"
             zIndex={1000000}
           >
-            <ScrollArea h="calc(100vh - 80px" mx="-md">
+            <ScrollArea mx="-md">
                 <Divider my="sm"/>
                 <Group className='divideritems'>
                 <Link to="/" className='itemdivider' style={{backgroundColor: pathname ==='/' ? 'lightgray': 'white'}}>Home</Link>
